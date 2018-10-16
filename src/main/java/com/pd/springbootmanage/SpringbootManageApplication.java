6e48e2bd4061dfdc8d5e27325f7177387029384a
wanxuanwen <wanxuanwen@able-elec.com>
2018-10-16 20:57:50 +0800

    新增druid数据源以及Pagehelper插件

--- src/main/java/com/pd/springbootmanage/SpringbootManageApplication.java
+++ src/main/java/com/pd/springbootmanage/SpringbootManageApplication.java
@@ -1,5 +1,6 @@
 package com.pd.springbootmanage;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author able
  */
 @SpringBootApplication
+@MapperScan("com.pd.manage.dao")
 public class SpringbootManageApplication {
 
 	public static void main(String[] args) {
