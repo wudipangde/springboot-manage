4bbb8d29df5b599b10810867cbe03468c1435754
wanxuanwen <wanxuanwen@able-elec.com>
2018-10-16 19:53:45 +0800

    Initial commit

---
+++ src/main/java/com/pd/springbootmanage/SpringbootManageApplication.java
@@ -0,0 +1,15 @@
+package com.pd.springbootmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author able
+ */
+@SpringBootApplication
+public class SpringbootManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootManageApplication.class, args);
+	}
+}
