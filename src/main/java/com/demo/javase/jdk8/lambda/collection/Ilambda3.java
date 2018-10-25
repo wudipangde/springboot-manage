b5a9b1e0d19aae59af67ec60677aff9f640bb2ed
wudipangde <961046863@qq.com>
2018-10-25 13:22:52 +0800

    增加lambda表达式

---
+++ src/main/java/com/demo/javase/jdk8/lambda/collection/Ilambda3.java
@@ -0,0 +1,13 @@
+package com.demo.javase.jdk8.lambda.collection;
+
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author wxw
+ * @Description:XX
+ * @date 2018/10/22
+ */
+public interface Ilambda3 {
+	abstract void sort(List<Integer> list, Comparator<Integer> c);
+}
