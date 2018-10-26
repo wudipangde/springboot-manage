1b265f4f08149b8c48ee1caec610ddeffc1d73d3
wudipangde <961046863@qq.com>
2018-10-26 08:42:16 +0800

    引入redis

---
+++ src/main/java/com/pd/manage/service/system/IRoleService.java
@@ -0,0 +1,9 @@
+package com.pd.manage.service.system;
+
+/**
+ * @author wxw
+ * @Description:XX
+ * @date 2018/10/25
+ */
+public interface IRoleService {
+}
---
+++ src/main/java/com/pd/manage/service/system/impl/RoleServiceImpl.java
@@ -0,0 +1,9 @@
+package com.pd.manage.service.system.impl;
+
+/**
+ * @author wxw
+ * @Description:XX
+ * @date 2018/10/25
+ */
+public class MenuServiceImpl {
+}
